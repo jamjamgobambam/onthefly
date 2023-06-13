274fd030599b74ebaade4afb2cc0794eb86b59ce
Jamila Cocchiola <82185575+jamjamgobambam@users.noreply.github.com>
2023-06-12 22:22:32 -0700

    update vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// Default configuration
+const defaultConfig = {
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://onthefly-server.up.railway.app/',
-        changeOrigin: true
-      },
-      '/auth': {
-        target: 'https://onthefly-server.up.railway.app/',
-        changeOrigin: true
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const isDev = mode === 'development'
+
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: isDev ? 'http://localhost:3001' : 'https://onthefly-server.up.railway.app',
+          },
+          '/auth': {
+            target: isDev ? 'http://localhost:3001' : 'https://onthefly-server.up.railway.app',
+          }
+        }
       }
     }
+  } else {
+    return defaultConfig
   }
 })
