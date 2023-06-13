438e0196a5c6efc539d9a7018a11e9c8fa68d088
Jamila Cocchiola <82185575+jamjamgobambam@users.noreply.github.com>
2023-06-12 23:00:54 -0700

    update server cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(session({
 // middleware
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://onthefly.up.railway.app',
     methods: 'GET,POST,PUT,DELETE,PATCH',
     credentials: true
   }
